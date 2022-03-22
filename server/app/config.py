7751347cf02c323a59bc7cd7d9ce7d8f85e70be2
mhkc <markus.johansson@me.com>
2022-03-22 16:30:52 +0100

    Added option to configure API CORS

--- server/app/config.py
+++ server/app/config.py
@@ -1,9 +1,16 @@
 """Mimer api default configuration"""
+import os
 # Database connection
 # standard URI has the form:
 # mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
 # read more: https://docs.mongodb.com/manual/reference/connection-string/
-DATABASE_NAME = "mimer"
-MONGODB_URI = f"mongodb://mongodb/{DATABASE_NAME}"
-MAX_CONNECTIONS = 10
-MIN_CONNECTIONS = 10
+DATABASE_NAME = os.getenv("DATABASE_NAME", "mimer")
+DB_HOST = os.getenv("DB_HOST", "mongodb")
+DB_PORT = os.getenv("DB_PORT", "27017")
+MONGODB_URI = f"mongodb://{DB_HOST}:{DB_PORT}/{DATABASE_NAME}"
+MAX_CONNECTIONS = int(os.getenv("MAX_CONNECTIONS", 10))
+MIN_CONNECTIONS = int(os.getenv("MIN_CONNECTIONS", 10))
+
+# Configure allowed origins (CORS) for development. Origins are a comma seperated list.
+# https://fastapi.tiangolo.com/tutorial/cors/
+allowed_origins = os.getenv('ALLOWED_ORIGINS', '').split(',')
--- server/app/main.py
+++ server/app/main.py
@@ -3,11 +3,19 @@
 from fastapi import Depends, FastAPI
 from .routers import groups, samples, users, locations
 from .db.utils import close_mongo_connection, connect_to_mongo
+from .internal.middlewares import configure_cors
+
 
 app = FastAPI(title="Mimer")
 
+# configure CORS
+configure_cors(app)
+
+# configure events
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
+
+# add api routes
 app.include_router(groups.router)
 app.include_router(samples.router)
 app.include_router(users.router)
