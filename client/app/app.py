4f8f6de144e6c1c5ff6f77b8069c8f1bf2764a6d
mhkc <markus.johansson@me.com>
2022-07-27 16:46:47 +0200

    WIP basic project layout

--- client/app/app.py
+++ client/app/app.py
@@ -1,13 +1,19 @@
 """Code for setting up the flask app."""
 from flask import Flask, current_app
+from .blueprints import public
 
 def create_app():
     """Flask app factory function."""
 
     app = Flask(__name__)
+    # load default config
+    app.config.from_pyfile("config.py")
 
-    @app.route("/")
-    def home():
-        return "<p>Hello World</p>"
+    register_blueprints(app)
 
     return app
+
+
+def register_blueprints(app):
+    """Register flask blueprints."""
+    app.register_blueprint(public.public_bp)
