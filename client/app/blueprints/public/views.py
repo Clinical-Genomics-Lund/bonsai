2bef5f51257b0391f3238c6d80775168d7eca4ec
mhkc <markus.johansson@me.com>
2022-07-27 16:46:47 +0200

    WIP basic project layout

---
+++ client/app/blueprints/public/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, current_app, render_template, send_from_directory
+from app import VERSION
+
+public_bp = Blueprint("public", __name__, template_folder="templates", static_folder="static")
+
+@public_bp.route("/")
+def index():
+    """Landing page view."""
+
+    return render_template("index.html", version=VERSION)
+
+@public_bp.route("/favicon")
+def favicon():
+    return send_from_directory(current_app.static_folder, "favicon.ico")
