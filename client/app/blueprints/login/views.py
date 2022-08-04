d4599ba74c1e73755aaadb319487c9f6f7450abc
mhkc <markus.johansson@me.com>
2022-08-04 15:19:13 +0200

    Added login error messages

--- client/app/blueprints/login/views.py
+++ client/app/blueprints/login/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, session, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, UserMixin
 from app.extensions import login_manager
 from app.mimer import get_auth_token, get_current_user, TokenObject
+from requests.exceptions import HTTPError
 
 login_bp = Blueprint("login", __name__, template_folder="templates", static_folder="static")
 
@@ -46,7 +47,20 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    token_obj: TokenObject = get_auth_token(username, password)
+    try:
+        token_obj: TokenObject = get_auth_token(username, password)
+    except HTTPError as err:
+        # if invalid credentials
+        if err.response.status_code == 401:
+            flash("Invalid login credentials", "danger")
+        else:
+            # TODO redirect to report bug page
+            # TODO log errors...
+            flash("Sorry, you could not log in due to an internal error", "warning")
+
+        return redirect(url_for("public.index"))
+
+
     user_obj = get_current_user(token_obj)
     user = LoginUser(user_obj, token_obj)
     return perform_login(user)
@@ -56,7 +70,11 @@ def login():
 def load_user(user_id):
     """Reload user object from user id stored in session."""
     token = TokenObject(**user_id)
-    user_obj = get_current_user(token)
+    try:
+        user_obj = get_current_user(token)
+    except HTTPError as error:
+        return None
+
     user = LoginUser(user_obj, token) if user_obj else None
     return user
 
