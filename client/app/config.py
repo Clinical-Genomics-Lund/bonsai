a5f6b00a423b8306961b32f72c3f56fab965c2b5
mhkc <markus.johansson@me.com>
2022-08-25 14:58:15 +0200

    Client now reads api url from env

--- client/app/config.py
+++ client/app/config.py
@@ -1,2 +1,8 @@
+"""Configuration of web site"""
+import os
+
+# Setup api url
+MIMER_API_URL = os.getenv("MIMER_API_URL", "http://api:8000")
+
+# Session secret key
 SECRET_KEY = b"not-so-secret"
-MIMER_API_URL = "http://api:8000"
