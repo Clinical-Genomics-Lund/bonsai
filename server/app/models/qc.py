51f94d5e653600ca486c5002b95a751fd12a9700
mhkc <markus.johansson@me.com>
2023-06-08 10:07:05 +0200

    Added support for PostAlignQc results to api

--- server/app/models/qc.py
+++ server/app/models/qc.py
@@ -1,16 +1,17 @@
 """QC data models."""
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from .base import RWModel
 
 
-class QcTool(Enum):
+class QcSoftware(Enum):
     """Valid tools."""
 
     QUAST = "quast"
     FASTQC = "fastqc"
+    POSTALIGNQC = "postalignqc"
 
 
 class QuastQcResult(BaseModel):
@@ -26,7 +27,21 @@ class QuastQcResult(BaseModel):
     duplication_ratio: float
 
 
+class PostAlingQcResult(BaseModel):
+    """PostAlignQc workflow results."""
+
+    ins_size: int = Field(..., description="Average insert size")
+    ins_size_dev: int = Field(..., description="Insert size standard deviation")
+    mean_cov: int = Field(..., description="Average read coverage")
+    pct_above_x: dict[str, float] = Field(..., description="Read coverage distribution")
+    mapped_reads: int = Field(..., description="Number of mapped reads")
+    tot_reads: int
+    iqr_median: float
+    dup_pct: float = Field(..., description="Percentage of duplicated reads")
+    dup_reads: int = Field(..., description="Number of duplicated reads")
+
+
 class QcMethodIndex(RWModel):
-    software: QcTool
+    software: QcSoftware
     version: str | None
-    result: QuastQcResult
+    result: QuastQcResult | PostAlingQcResult
