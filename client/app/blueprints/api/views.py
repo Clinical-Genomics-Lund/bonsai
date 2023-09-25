e1702e99e593383886c5178cbffbab5f1b13f92e
Alexander Koc <alexander.koc@skane.se>
2023-09-25 15:17:51 +0200

    Add possibility to remove all samples to clientside api

--- client/app/blueprints/api/views.py
+++ client/app/blueprints/api/views.py
@@ -2,8 +2,11 @@
 from flask import Blueprint, request, session, jsonify
 from flask_login import login_required, current_user
 import json
+import logging
 
 api_bp = Blueprint("api", __name__, template_folder="templates", static_folder="static")
+
+LOG = logging.getLogger(__name__)
 
 
 @api_bp.route("/api/basket/add", methods=["POST"])
@@ -21,10 +24,11 @@ def add_sample_to_basket():
         # add only unique id
         session["basket"] = list(
             {
-                entry["sample_id"]: entry for entry in samples_in_basket + samples_to_add
+                entry["sample_id"]: entry
+                for entry in samples_in_basket + samples_to_add
             }.values()
         )
-        msg = ', '.join([entry["sample_id"] for entry in samples_in_basket])
+        msg = ", ".join([entry["sample_id"] for entry in samples_in_basket])
         return f"Added {msg}", 200
 
 
@@ -39,8 +43,15 @@ def remove_sample_from_basket():
     if request.method == "POST":
         # add samples to basket
         samples_in_basket = session.get("basket")
-        to_remove = json.loads(request.data).get("sample_id", "")
-        samples = [sid for sid in samples_in_basket if not sid["sample_id"] == to_remove]
-        session["basket"] = samples
+        request_data = json.loads(request.data)
+        if request_data.get("remove_all", False):
+            to_remove = str(session["basket"])
+            session["basket"] = []
+        else:
+            to_remove = json.loads(request.data).get("sample_id", "")
+            samples = [
+                sid for sid in samples_in_basket if not sid["sample_id"] == to_remove
+            ]
+            session["basket"] = samples
         return f"removed {to_remove}", 200
     return "", 200
