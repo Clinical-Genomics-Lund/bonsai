884e33a57287deb2b0e96a08c9e82914187d99db
mhkc <markus.johansson@me.com>
2023-10-06 16:28:29 +0200

    Fixed issues with api pyproject.toml

--- server/setup.py
+++ server/setup.py
@@ -1,31 +1,5 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import setuptools
 
-setup(
-    name="bonsai_api",
-    version="0.1",
-    description="Bonsai REST api",
-    author="Markus Johansson",
-    author_email="markus.johansson@me.com",
-    packages=find_packages(),
-    python_requires=">=3.6",
-    install_requires=[
-        "setuptools",
-        "wheel",
-        "click",
-        "motor",
-        "fastapi[all]",
-        "uvicorn[standard]",
-        "python-jose[cryptography]",
-        "python-multipart",
-        "passlib",
-        "ldap3",
-        "pandas",
-        "sourmash",
-        "biopython",
-        "scipy",
-        "scikit-bio",
-        "pydantic-factories",
-        "grapetree",
-    ],
-    entry_points={"console_scripts": ["bonsai_api = app.cli:cli"]},
-)
+if __name__ == "__main__":
+    setuptools.setup()
