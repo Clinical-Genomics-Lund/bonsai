09bf432bdfad117e8363687f0c89a89aefd00cdf
mhkc <markus.johansson@me.com>
2023-10-30 11:24:08 +0100

    tree -> db

--- minhash_service/app/minhash/similarity.py
+++ minhash_service/app/minhash/similarity.py
@@ -61,6 +61,7 @@ def add_signatures_to_index(sample_ids: List[str]) -> bool:
 
     return True
 
+
 def get_similar_signatures(sample_id: str, min_similarity: float, limit: int | None = None) -> SimilarSignatures:
     """Get find samples that are similar to reference sample.
 
@@ -71,7 +72,7 @@ def get_similar_signatures(sample_id: str, min_similarity: float, limit: int | N
     LOG.debug(f'Getting samples similar to: {sample_id}')
     index_path: pathlib.Path = get_sbt_index()
     LOG.debug(f'Load index file to memory')
-    tree = sourmash.load_file_as_index(index_path)
+    db = sourmash.load_file_as_index(index_path)
 
     # load reference sequence
     query_signature = _load_signature(sample_id)
@@ -82,7 +83,7 @@ def get_similar_signatures(sample_id: str, min_similarity: float, limit: int | N
 
     # query for similar sequences
     LOG.debug(f'Searching for signatures with similarity > {min_similarity}')
-    result = tree.search(query_signature, threshold=min_similarity) # read sample information of similar samples 
+    result = db.search(query_signature, threshold=min_similarity) # read sample information of similar samples 
     samples = []
 
     for itr_no, (similarity, found_sig, _) in enumerate(result, start=1):
