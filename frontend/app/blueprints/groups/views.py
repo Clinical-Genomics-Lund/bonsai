424545de49dbc75ba86722565cc9f2b296207d13
Alexander Koc <alexander.koc@skane.se>
2023-11-21 11:54:24 +0100

    Add routing for bulk sample qc status setting
    Isort imports.

--- frontend/app/blueprints/groups/views.py
+++ frontend/app/blueprints/groups/views.py
@@ -2,6 +2,10 @@
 import json
 import logging
 
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+from flask_login import current_user, login_required
+from requests.exceptions import HTTPError
+
 from app.bonsai import (
     TokenObject,
     create_group,
@@ -11,11 +15,9 @@ from app.bonsai import (
     get_samples_by_id,
     get_samples_in_group,
     update_group,
+    update_sample_qc_classification,
 )
 from app.models import PhenotypeType
-from flask import Blueprint, flash, redirect, render_template, request, session, url_for
-from flask_login import current_user, login_required
-from requests.exceptions import HTTPError
 
 LOG = logging.getLogger(__name__)
 
@@ -141,3 +143,44 @@ def group(group_id: str) -> str:
         modified=group_info["modified_at"],
         table_definition=table_definition,
     )
+
+
+@groups_bp.route("/groups/qc_status", methods=["POST"])
+@login_required
+def update_qc_classification():
+    """Update the quality control report of one or more samples."""
+
+    selected_samples = request.form.getlist("qc-selected-samples")
+
+    if not selected_samples:
+        raise ValueError(
+            "No selected samples found in the request. "
+            "Ensure that 'selectedSamples' is provided and contains at least one sample."
+        )
+
+    token = TokenObject(**current_user.get_id())
+
+    # build data to store in db
+    result = request.form.get("qc-validation", None)
+    if result == "passed":
+        action = None
+        comment = ""
+    elif result == "failed":
+        comment = request.form.get("qc-comment", "")
+        action = request.form.get("qc-action", "")
+    else:
+        raise ValueError(f"Unknown value of qc classification, {result}")
+
+    for sample_id in selected_samples:
+        try:
+            update_sample_qc_classification(
+                token,
+                sample_id=sample_id,
+                status=result,
+                action=action,
+                comment=comment,
+            )
+        except Exception as error:
+            flash(str(error), "danger")
+
+    return redirect(url_for("groups.groups"))
