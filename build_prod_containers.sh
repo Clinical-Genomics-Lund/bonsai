52988c74c14724f1239960604e9e2f5f682078e5
mhkc <markus.johansson@me.com>
2024-01-09 16:35:57 +0100

    removed files that were accidentaly added

--- build_prod_containers.sh
+++
@@ -1,9 +0,0 @@
-# build
-docker build --no-cache -t clinicalgenomicslund/bonsai_app:0.10 frontend
-docker push clinicalgenomicslund/bonsai_app:0.1.0
-docker build --no-cache -t clinicalgenomicslund/bonsai_api:0.10 api
-docker push clinicalgenomicslund/bonsai_api:0.1.0
-docker build --no-cache -t clinicalgenomicslund/bonsai_minhash_service:0.1.0 minhash_service
-docker push clinicalgenomicslund/bonsai_minhash_service:0.1.0
-docker build --no-cache -t clinicalgenomicslund/bonsai_allele_cluster_service:0.1.0 allele_cluster_service
-docker push clinicalgenomicslund/bonsai_allele_cluster_service:0.1.0
