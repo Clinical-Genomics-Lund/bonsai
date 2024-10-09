85ed338fbd5e53ab04329d7a0145e9ec727afd7c
mhkc <markus.johansson@me.com>
2024-10-09 15:29:10 +0200

    Use api entrypoint to bootstrap e2e database

--- ete_tests/setup_bonsai_database.sh
+++ ete_tests/setup_bonsai_database.sh
@@ -2,7 +2,8 @@
 set -e
 
 # bootstrap bonsai database
-bonsai_api setup --help
+bonsai_api setup
+bonsai_api create-user -u user -p user -m user@mail.com -r user
 
 # run commands given to container
 echo "Executing command: ${@}"
