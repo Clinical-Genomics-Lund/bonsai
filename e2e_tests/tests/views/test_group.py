466962bc7a3160c3f1a929cb655b83e638410141
mhkc <markus.johansson@me.com>
2024-10-21 15:34:12 +0200

    Add test for adding multiple samples to basket

--- e2e_tests/tests/views/test_group.py
+++ e2e_tests/tests/views/test_group.py
@@ -2,8 +2,13 @@
 
 import pytest
 from pathlib import Path
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
-from ..conftest import get_element_by_test_id
+from ..conftest import get_bootstrap_alert, get_element_by_test_id
 
 
 @pytest.mark.parametrize("group_id", ["mtuberculosis", "saureus", "ecoli"])
@@ -31,3 +36,53 @@ def test_open_qc_view(logged_in_user, config, group_id: str):
 
     # TEST that the page could load
     assert "bonsai" in logged_in_user.title.lower()
+
+
+@pytest.mark.parametrize("group_id", ["mtuberculosis", "saureus", "ecoli"])
+def test_add_samples_to_basket(logged_in_user, config, group_id: str):
+    """Test the QC view could be opended for the different test groups."""
+
+    # setup wait
+    wait = WebDriverWait(logged_in_user, 10)
+
+    # FIRST goto the group view
+    url = Path(config["frontend_url"]) / 'groups' / group_id
+    logged_in_user.get(str(url))
+
+    # THEN ensure that basket has been cleared
+    get_element_by_test_id(logged_in_user, "open-basket-btn").click()
+    clear_basket_btn = wait.until(
+        EC.visibility_of_element_located((By.ID, "clear-basket-btn"))
+    )
+    clear_basket_btn.click()
+    get_element_by_test_id(logged_in_user, "close-basket-btn").click()
+
+    # THEN select the first five samples
+    (
+        ActionChains(logged_in_user)
+        .key_down(Keys.CONTROL)  # hold controll
+        .click(on_element = get_element_by_test_id(logged_in_user, f"sample-row-1"))  # select multiple samples
+        .click(on_element = get_element_by_test_id(logged_in_user, f"sample-row-2"))
+        .click(on_element = get_element_by_test_id(logged_in_user, f"sample-row-3"))
+        .click(on_element = get_element_by_test_id(logged_in_user, f"sample-row-4"))
+        .click(on_element = get_element_by_test_id(logged_in_user, f"sample-row-5"))
+        .key_up(Keys.CONTROL)
+    ).perform()
+    
+    # THEN check that the add to basket button has been enabled
+    add_to_basket_btn = get_element_by_test_id(logged_in_user, "add-to-basket-btn")
+    assert add_to_basket_btn.is_enabled()
+
+    # THEN click the button
+    add_to_basket_btn.click()
+
+    # wait for page to load
+    logged_in_user.implicitly_wait(2)
+
+    # TEST that no error alerts were thrown
+    alert = get_bootstrap_alert(logged_in_user)
+    assert alert is None, f"Alert error: {alert.text}"
+
+    # TEST that one sample has been added to the basket
+    counter = get_element_by_test_id(logged_in_user, "samples-in-basket-counter")
+    assert counter.text == "5"
