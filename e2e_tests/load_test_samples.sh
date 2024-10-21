2029edb6b86992bbc9105a4c0ad0edb01f25cfad
mhkc <markus.johansson@me.com>
2024-10-21 15:32:44 +0200

    Generic function to load all fixture samples

--- e2e_tests/load_test_samples.sh
+++ e2e_tests/load_test_samples.sh
@@ -2,9 +2,4 @@
 
 # Load test samples
 # =================
-
-# M. tuberculosis
-/home/worker/upload_sample.py --user admin --password admin --api http://api:8000/ --input /home/worker/fixtures/samples/tb_test_1_bonsai.yaml 
-
-# S. aureus
-/home/worker/upload_sample.py --user admin --password admin --api http://api:8000/ --input /home/worker/fixtures/samples/cmdtest1_240918_nb000000_0000_test_bonsai.yaml
+find ~/fixtures/samples/ -name *yaml -exec ~/upload_sample.py --user admin --password admin --api http://api:8000/ --input {} \;
