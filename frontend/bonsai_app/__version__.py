0ab51de37b6632f6330dec738fcdab5d7f322bd8
Markus Johansson <markus.h.johansson@skane.se>
2024-10-28 09:20:02 +0100

    Bumped sw versions

--- frontend/bonsai_app/__version__.py
+++ frontend/bonsai_app/__version__.py
@@ -1,2 +1,2 @@
 """Bonsai frontend version."""
-VERSION = "0.7.0"
+VERSION = "0.8.0"
--- minhash_service/minhash_service/__init__.py
+++ minhash_service/minhash_service/__init__.py
@@ -1,3 +1,3 @@
 """Service to perform MinHash based analysis using Sourmash."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
